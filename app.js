0ffa54ce4c91929bd6d3af757a3d8de74fa3659f
Arkadiusz Wieczorek <kontakt@arkadiusz-wieczorek.pl>
2016-04-04 09:27:04 +0200

    Fix code

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ function createRandomOrderForImages() {
     filenames.sort(function() {
         return .5 - Math.random();
     });
+    console.log(filenames)
 }
 
 function getThreeElements() {
@@ -30,6 +31,10 @@ function getThreeElements() {
 function changeImageSources() {
     var filenames = getThreeElements();
     if (round === 1) {
+        var header = document.querySelector('header');
+        var h2 = document.createElement('h2');
+        h2.textContent = "Round " + round;
+        header.appendChild(h2)
         var container = document.querySelector(".container");
         for (var i = 0; i < 3; i++) {
             var img = document.createElement('img')
@@ -38,17 +43,19 @@ function changeImageSources() {
             container.appendChild(img);
         }
     }
-    console.log(filenames)
+    console.log('images', filenames.toString())
     for (var i = 0; i < 3; i++) {
         var img = document.querySelector("#img" + i);
-        img.src = "img/" + filenames[i] + ".png";
+        img.src = "img/" + filenames[i] + ".jpg";
+        var h2 = document.querySelector('h2');
+        h2.textContent = "Round " + round;
     }
 }
 
 function nextRound() {
     if (round < 17) {
         changeImageSources();
-        console.log('round', round)
+        console.log('round', round);
         round = round + 1;
     } else {
         resetGame();
@@ -56,10 +63,14 @@ function nextRound() {
 }
 
 function resetGame() {
-    createRandomOrderForImages()
+    var h2 = document.querySelector('h2');
+    if (h2 !== null) h2.parentNode.removeChild(h2);
     for (var i = 0; i < 3; i++) {
-        var img = document.getElementById('img' + i)
-        img.parentNode.removeChild(img);
+        var img = document.getElementById('img' + i);
+        if(img !== null) img.parentNode.removeChild(img);
     }
     round = 1;
+    posititon = 0;
+    filenames = [];
+    createRandomOrderForImages()
 }
